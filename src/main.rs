5e9c9b60baca717a6c90b579f03f826e32fe3d17
Andrew Francis <ajfrancis102@gmail.com>
2022-05-07 13:10:14 -0400

    Added attribute limit checking

--- src/main.rs
+++ src/main.rs
@@ -20,24 +20,53 @@ impl Character {
         let mut name = String::new();
         let mut health = String::new();
         let mut strength = String::new();
+        let mut attribute_points_to_spend = 10;
 
         print!("Enter your name:");
         io::stdout().flush().expect("Flush didn't work");
         io::stdin().read_line(&mut name).expect("Failed to read line");
 
-        print!("Enter health attribute:");
-        io::stdout().flush().expect("Flush didn't work");
-        io::stdin().read_line(&mut health).expect("Failed to read line");
+        println!("You have {} attribute points to spend", attribute_points_to_spend);
 
-        print!("Enter strength attribute:");
-        io::stdout().flush().expect("Flush didn't work");
-        io::stdin().read_line(&mut strength).expect("Failed to read line");
+        let mut parsed_health: i32;
+
+        loop {
+            print!("Enter health attribute:");
+            io::stdout().flush().expect("Flush didn't work");
+            io::stdin().read_line(&mut health).expect("Failed to read line");
+            parsed_health = health.trim().parse().expect("Health is not a number");
+
+            if parsed_health <= attribute_points_to_spend {
+                attribute_points_to_spend -= parsed_health;
+                break;
+            } else {
+                println!("Entered health must be less than available attributes");
+                health.clear();
+            }
+        }
+
+        println!("You have {} attribute points to spend", attribute_points_to_spend);
+
+        let mut parsed_strength: i32;
+        loop {
+            print!("Enter strength attribute:");
+            io::stdout().flush().expect("Flush didn't work");
+            io::stdin().read_line(&mut strength).expect("Failed to read line");
+            parsed_strength = strength.trim().parse().expect("Strength is not a number");
+
+            if parsed_strength <= attribute_points_to_spend {
+                break;
+            } else {
+                println!("Entered strength must be less than available attributes");
+                strength.clear();
+            }
+        }
 
         return Character {
             name: String::from(name.trim()),
             money: 0,
-            health: health.trim().parse::<i32>().expect("Not a number"),
-            strength: strength.trim().parse::<i32>().expect("Not a number"),
+            health: parsed_health,
+            strength: parsed_strength,
         };
     }
 }
