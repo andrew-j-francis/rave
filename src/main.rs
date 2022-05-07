2a87f586ee522d773a116fa42fc37b7c8cbcde1e
Andrew Francis <ajfrancis102@gmail.com>
2022-05-07 12:56:37 -0400

    Implemented Character struct

--- src/main.rs
+++ src/main.rs
@@ -3,24 +3,41 @@ use std::io::{Write};
 
 fn main() {
     println!("Welcome to Rave!");
-    let character: (String, String) = create_character();
-
-    println!("{}{}", character.0, character.1);
+    let mut character = Character::create_character();
+    println!("Created character: {:#?}", character);
 }
 
-fn create_character() -> (String, String) {
-    let mut name = String::new();
-    let mut class = String::new();
+#[derive(Debug)]
+struct Character {
+    name: String,
+    money: i32,
+    health: i32,
+    strength: i32,
+}
 
-    print!("Enter your name:");
-    io::stdout().flush().expect("Flush didn't work");
-    io::stdin().read_line(&mut name).expect("Failed to read line");
+impl Character {
+    fn create_character() -> Character {
+        let mut name = String::new();
+        let mut health = String::new();
+        let mut strength = String::new();
 
-    println!("=== Select a Class ===");
-    println!("1. Warrior");
-    println!("2. Cleric");
-    println!("3. Rogue");
-    io::stdin().read_line(&mut class).expect("Failed to read line");
+        print!("Enter your name:");
+        io::stdout().flush().expect("Flush didn't work");
+        io::stdin().read_line(&mut name).expect("Failed to read line");
 
-    return (name, class);
+        print!("Enter health attribute:");
+        io::stdout().flush().expect("Flush didn't work");
+        io::stdin().read_line(&mut health).expect("Failed to read line");
+
+        print!("Enter strength attribute:");
+        io::stdout().flush().expect("Flush didn't work");
+        io::stdin().read_line(&mut strength).expect("Failed to read line");
+
+        return Character {
+            name: String::from(name.trim()),
+            money: 0,
+            health: health.trim().parse::<i32>().expect("Not a number"),
+            strength: strength.trim().parse::<i32>().expect("Not a number"),
+        };
+    }
 }
