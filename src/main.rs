74d1bb36a5d72f0e8a67385be2c9b5d5bb807745
Andrew Francis <ajfrancis102@gmail.com>
2022-05-10 18:07:20 -0400

    Created entity to replace character

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 mod character;
+mod entity;
 
 use std::io;
 use std::io::{Write};
@@ -13,59 +14,7 @@ fn main() {
     let mut siv = cursive::default();
     siv.load_toml(include_str!("style.toml")).unwrap();
 
-    siv.add_layer(
-        Dialog::around(
-            LinearLayout::vertical()
-                .child(TextView::new("Character Name"))
-                .child(EditView::new().with_name("character_name"))
-                .child(DummyView.fixed_height(1))
-                .child(TextView::new("Strength"))
-                .child(
-                    LinearLayout::horizontal()
-                        .child(TextView::new("0").with_name("strength_amount"))
-                        .child(DummyView.fixed_width(1))
-                        .child(
-                            SliderView::horizontal(11)
-                                .on_change(|s, v| {
-                                    s.call_on_name("strength_amount", |view: &mut TextView| {
-                                        view.set_content(v.to_string());
-                                    });
-                                })
-                        )
-                )
-                .child(TextView::new("Stamina"))
-                .child(
-                    LinearLayout::horizontal()
-                        .child(
-                            TextView::new("0").with_name("stamina_amount")
-                        )
-                        .child(DummyView.fixed_width(1))
-                        .child(
-                            SliderView::horizontal(11)
-                                .on_change(|s, v| {
-                                    s.call_on_name("stamina_amount", |view: &mut TextView| {
-                                        view.set_content(v.to_string());
-                                    });
-                                })
-                        )
-                )
-                .fixed_width(30),
-        )
-            .title("Create Character")
-            .button("Create", |s| {
-                let character_name = s.call_on_name("character_name", |view: &mut EditView| {
-                    view.get_content().to_string()
-                });
-
-                let stamina = s.call_on_name("stamina_amount", |view: &mut SliderView| {
-                    view.get_value()
-                });
-
-                test(&character_name.unwrap(), &stamina.unwrap());
-            })
-            .h_align(HAlign::Left),
-    );
-
+    entity::create_player(&mut siv);
     siv.run();
 }
 
